8e70f540b7e8747a5160aa8dd5bbac483fa20767
aminiconi <aminiconi@kangourouge.com>
2018-05-03 18:23:12 +0200

    [FIX] Working sortable action

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -37,34 +37,14 @@ class AdminController extends \EasyCorp\Bundle\EasyAdminBundle\Controller\AdminC
      */
     protected function sortAction()
     {
-        $id = $this->request->query->get('id');
         $relativePosition = $this->request->query->get('relativePosition');
         $field = 'position';
         $getter = 'get'.ucfirst($field);
         $setter = 'set'.ucfirst($field);
-
         $easyadmin = $this->request->attributes->get('easyadmin');
         $entity = $easyadmin['item'];
         $currentPosition = $entity->$getter();
         $newPosition = $currentPosition + $relativePosition;
-
-        // Find impacted items
-        $qb = $this->em->createQueryBuilder()
-           ->select('e')
-           ->from($easyadmin['entity']['class'], 'e')
-           ->where('e.position >= :floor')
-           ->andWhere('e.position <= :top')
-           ->andWhere('e.id != :id')
-           ->setParameters([
-               'floor' => $currentPosition < $newPosition ? $currentPosition : $newPosition,
-               'top'   => $currentPosition > $newPosition ? $currentPosition : $newPosition,
-               'id'    => $id,
-           ]);
-
-        // Move other items
-        foreach ($qb->getQuery()->getResult() as $entry) {
-            $entry->setPosition($entry->$getter() + (($entry->$getter() < $newPosition || ($entry->$getter() === $newPosition && $newPosition > $currentPosition)) ? -1 : 1));
-        }
         $entity->$setter($newPosition);
         $this->em->flush();
 
