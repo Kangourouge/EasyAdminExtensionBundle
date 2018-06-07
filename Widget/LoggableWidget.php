5b4d827fb4c62ad61a7fb16f5abef83254d1fa01
aminiconi <aminiconi@kangourouge.com>
2018-06-07 11:02:41 +0200

    [U] Stats template

--- Widget/LoggableWidget.php
+++ Widget/LoggableWidget.php
@@ -9,22 +9,12 @@ use Symfony\Component\Templating\EngineInterface;
 
 class LoggableWidget implements WidgetInterface
 {
-    /**
-     * @var EntityManagerInterface
-     */
+    /** @var EntityManagerInterface */
     protected $entityManager;
 
-    /**
-     * @var EngineInterface
-     */
+    /** @var EngineInterface */
     protected $templating;
 
-    /**
-     * LoggableWidget constructor.
-     *
-     * @param EntityManagerInterface $entityManager
-     * @param EngineInterface $templating
-     */
     public function __construct(EntityManagerInterface $entityManager, EngineInterface $templating)
     {
         $this->entityManager = $entityManager;
@@ -33,18 +23,18 @@ class LoggableWidget implements WidgetInterface
 
     public function render()
     {
-        /** @var EntityRepository $repository */
-        $repository = $this->entityManager->getRepository(LogEntry::class);
-        $data = $repository
-                        ->createQueryBuilder('l')
-                        ->select('l.username', 'l.action', 'l.loggedAt', 'l.objectClass')
-                        ->setMaxResults(50)
-                        ->orderBy('l.loggedAt', 'DESC')
-                        ->getQuery()
-                            ->getScalarResult();
+        $maxResults = 20;
+        $data = $this->entityManager->getRepository(LogEntry::class)
+                                    ->createQueryBuilder('l')
+                                    ->select('l.username', 'l.action', 'l.loggedAt', 'l.objectClass')
+                                    ->setMaxResults($maxResults)
+                                    ->orderBy('l.loggedAt', 'DESC')
+                                    ->getQuery()
+                                    ->getScalarResult();
 
         return $this->templating->render('KRGEasyAdminExtensionBundle:widget:loggable.html.twig', [
-            'data' => $data
+            'data'       => $data,
+            'maxResults' => $maxResults,
         ]);
     }
 }
